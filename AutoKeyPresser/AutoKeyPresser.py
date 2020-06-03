bde2b0b3e2fecc023e3de9635be8025cfb8290b1
TheEpicProgrammer <63802868+TheEpicProgrammer@users.noreply.github.com>
2020-06-03 14:43:14 -0500

    general improvements

--- AutoKeyPresser/AutoKeyPresser.py
+++ AutoKeyPresser/AutoKeyPresser.py
@@ -1,27 +1,29 @@
 from pynput.keyboard import Key, Controller
 import time
-
-keyboard = Controller()
-presskey = input("What Key to Spam Press???")
-times = float(input("how many times to press?? If infinity type -1"))
-delaytime = float(input("What delay Between Pressing Keys?(seconds)"))
-input("press enter to continue")
-
-
-for count in range(3, 0, -1):
-    print(count)
-    time.sleep(1)
+try:
+    keyboard = Controller()
+    presskey = input("What Key to Spam Press???\n>")
+    times = float(input("how many times to press?? If infinity type -1\n>"))
+    delaytime = float(input("What delay Between Pressing Keys?(seconds)\n>"))
+    input("press enter to continue")
 
 
-if times == -1:
-    while True:
-        keyboard.press(presskey)
-        keyboard.release(presskey)
-        #keyboard.send(delaytime)
-        time.sleep(delaytime)
-elif times >= 0:
-    for number in range(times):
-        keyboard.press(presskey)
-        keyboard.release(presskey)
-        #keyboard.send(presskey)
-        time.sleep(delaytime)
+    for count in range(3, 0, -1):
+        print(count)
+        time.sleep(1)
+
+
+    if times == -1:
+        while True:
+            keyboard.press(presskey)
+            keyboard.release(presskey)
+            #keyboard.send(delaytime)
+            time.sleep(delaytime)
+    elif times >= 0:
+        for number in range(times):
+            keyboard.press(presskey)
+            keyboard.release(presskey)
+            #keyboard.send(presskey)
+            time.sleep(delaytime)
+except:
+	print("A error occured")
